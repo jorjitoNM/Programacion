26b6c43d84f8c228c854a0b018ab802b213441dc
jorjitoNM <jorgenovillo2005@gmail.com>
2024-01-09 17:08:04 +0100

    Clase 09/01/2024 (ejercicios POO1)

--- Evaluacion2/EjerciciosPOO1/Ej1/src/Circulo.java
+++ Evaluacion2/EjerciciosPOO1/Ej1/src/Circulo.java
@@ -1,3 +1,36 @@
 public class Circulo {
 
+    private Punto centro;
+    private double radio;
+
+    public Circulo () {
+        centro = new Punto();
+        radio = Math.random()*4+1;
+    }
+    public Circulo (double Coordenada1, double Coordenada2, double radio) {
+        centro = new Punto(Coordenada1,Coordenada2);
+        this.radio = radio;
+    }
+    public void setCentro (Punto punto){
+        this.centro = punto;
+    }
+    public void setRadio (double radio) {
+        this.radio = radio;
+    }
+    public Punto getCentro () {
+        return centro;
+    }
+    public double getRadio () {
+        return radio;
+    }
+    public double calcularDistancia (Punto punto) {
+        return (Punto.calcularDistancia(punto,centro))-radio;
+    }
+    public double calcularArea () {
+        return Math.PI*Math.pow(radio,2);
+    }
+    public double calcularPerímetro () {
+        return Math.PI*radio*2;
+    }
+
 }
--- Evaluacion2/EjerciciosPOO1/Ej1/src/Punto.java
+++ Evaluacion2/EjerciciosPOO1/Ej1/src/Punto.java
@@ -24,4 +24,7 @@ public class Punto {
     public void setCoordenada2 (double Coordenada2) {
         this.Coordenada2 = Coordenada2;
     }
+    public double calcularDistancia (Punto punto1,Punto punto2) {
+        return Math.sqrt((Math.pow(punto1.Coordenada2-punto1.Coordenada1,2))-(Math.pow(punto2.Coordenada2-punto2.Coordenada1,2)));
+    }
 }
